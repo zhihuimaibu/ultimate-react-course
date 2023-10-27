82f7aefd1a6c976fb3f15c6437888b179b02a7a3
wzh <zhihui258456zhihui@163.com>
2023-10-27 19:00:17 +0800

    ttt

--- challenge/src/accordion/Accordion.js
+++ challenge/src/accordion/Accordion.js
@@ -3,19 +3,19 @@ import "./Accordion.css";
 export default function Accordion() {
   const faqs = [
     {
-      id: 1,
+      id: '01',
       title: "Where are these chairs assembled?",
       text:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
     },
     {
-      id: 2,
+      id: '02',
       title: "How long do I have to return my chair?",
       text:
         "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
     },
     {
-      id: 3,
+      id: '03',
       title: "Do you ship to countries outside the EU?",
       text:
         "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
@@ -26,10 +26,12 @@ export default function Accordion() {
     <div className="accordion">
       {
         faqs.map(v => {
-          return <div className="item" key={v.id}>
-            <div className="number">{v.id}</div>
-            {/* <div className="title">{v.title}</div> */}
-            {v.text}
+          return <div className="item open " key={v.id}>
+            <div className="content-box">
+              <div className="number">{v.id}</div>
+              <div className="title text">{v.title}</div>
+              {v.text}
+            </div>
           </div>
         })
       }
